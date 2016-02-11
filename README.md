84c616ef6f34be0b9d0cf3da4efa181f97e17ec9
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2016-02-11 18:28:32 -0500

    origin();

---
+++ README.md
@@ -0,0 +1,14 @@
+# reverse
+
+Simple tool to buffer up and reverse the order of stdin lines, then print to stdout.
+
+```bash
+$ printf '1\n2\n3\n' | ./reverse
+3
+2
+1
+```
+
+_written in rust or something_
+
+[_LICENSE: ISC_](LICENSE)
---
+++ reverse.rs
@@ -0,0 +1,33 @@
+
+// imports
+use std::io::{self, Read};
+
+
+// program entry for reverse(1)
+fn main() {
+    // top-level error handler
+    match reverse() {
+        Err(value) => panic!(value),
+        _ => {}
+    }
+}
+
+
+fn reverse() -> io::Result<i32> {
+    // create a String buffer
+    let mut input = String::new();
+
+    // get stdin and make a synchronous handle to it
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    // read all of stdin into a String buffer
+    try!(handle.read_to_string(&mut input));
+
+    // transform the buffer into lines and then reverse those
+    for line in input.lines().rev() {
+        println!("{}", line);
+    }
+
+    Ok(0) // exit the process
+}
